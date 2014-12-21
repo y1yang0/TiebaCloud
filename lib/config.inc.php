c13de19a33cceff37f9156a9200de8f168d6fd60
racaljk <cthulhujk@gmail.com>
2014-12-21 09:06:45 +0800

    stable test;

--- lib/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php header("Content-Type: text/html;charset=utf-8");
-				define("DB_IP","127.0.0.1");
-				define("DB_USERNAME","root");
-				define("DB_PASSWORD","");
-				define("DB_NAME","new_tiebacloud");
-				define("ADMIN_PASSWORD","123");
-				define("ADMIN_NAME","admin");
-				?>
